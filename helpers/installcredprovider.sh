a883d360373f476c94b4354ad29daf4ab298576c
LAFAGE Jean-Baptiste <jean-baptiste.lafage@faurecia.com>
2022-03-09 16:19:14 +0100

    fix: force usage of release v0.1.28 for apps running in dotnet 2.x

--- helpers/installcredprovider.sh
+++ helpers/installcredprovider.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# DESCRIPTION: A simple shell script designed to fetch the latest version
+# DESCRIPTION: A simple shell script designed to fetch the version 0.1.28
 # of the artifacts credential provider plugin for dotnet and
 # install it into $HOME/.nuget/plugins.
 # SEE: https://github.com/Microsoft/artifacts-credprovider/blob/master/README.md
 
 REPO="Microsoft/artifacts-credprovider"
 FILE="Microsoft.NuGet.CredentialProvider.tar.gz"
-VERSION="latest"
+VERSION="v0.1.28"
 # URL pattern documented at https://help.github.com/en/articles/linking-to-releases as of 2019-03-29
-URI="https://github.com/$REPO/releases/$VERSION/download/$FILE"
+URI="https://github.com/$REPO/releases/download/$VERSION/$FILE"
 NUGET_PLUGIN_DIR="$HOME/.nuget/plugins"
 
 # Ensure plugin directory exists
